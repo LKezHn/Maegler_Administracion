b8066447c6087f7a17aa2a419eb5e07affba47d1
Luis Martinez <mluise2406@gmail.com>
2021-11-19 16:51:20 -0600

    Home and Login completed

--- Administration-page/src/app/home/home.component.ts
+++ Administration-page/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CompaniesService } from '../companies.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+  isInCompanies : boolean = true
+  isInOrders : boolean = false
+  isInDrivers : boolean = false
+
+  constructor(public cS: CompaniesService) { }
 
   ngOnInit(): void {
+  }
+
+  getCompanies(){
+    this.isInCompanies = true
+    this.cS.isInCompanyDetails = false;
+    this.isInDrivers = false
+    this.isInOrders = false
+  }
+
+  getOrders(){
+    this.isInOrders = true
+    this.cS.isInCompanyDetails = false;
+    this.isInDrivers = false
+    this.isInCompanies = false
+  }
+
+  getDrivers(){
+    this.isInDrivers = true
+    this.cS.isInCompanyDetails = false;
+    this.isInOrders = false
+    this.isInCompanies = false
+  }
+
+  getCompanyDetails(){
+    this.isInDrivers = false
+    this.isInOrders = false
+    this.isInCompanies = true
+    this.cS.isInCompanyDetails = true;
+  }
+
+  addCompany(){
+    this.isInDrivers = false
+    this.isInOrders = false
+    this.isInCompanies = false
+    this.cS.isInCompanyDetails = false;
+    this.cS.addCompany = true;
+  }
+
+  closeAddCompany($event: boolean){
+    this.isInCompanies = $event;
   }
 
 }
